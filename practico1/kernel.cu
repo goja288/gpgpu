5724bf2e975db2c0d4543ba11a36929d3ea5e240
goja288 <goja288@gmail.com>
2015-09-06 21:31:54 -0300

    Practico
    Compilando y funcionando en VS

---
+++ practico1/kernel.cu
@@ -0,0 +1,306 @@
+#include "cuda.h"
+#include "cuda_runtime.h"
+
+#include <stdio.h>
+#include <iostream>
+#include "util.h"
+using namespace std;
+
+//Kernel
+
+// Suma por columnas de una matriz con un solo bloque
+__global__ void MatrixSumKernel_1(int M, float* A_dev, float* SumPar_dev){
+
+	// Pvalue es usado para el valor intermedio
+	float Pvalue = 0;
+
+	int offset = threadIdx.y * M;
+
+	for (int k = 0; k < M; k++) {
+		Pvalue = Pvalue + A_dev[offset+k];
+	}
+
+	SumPar_dev[threadIdx.y] = Pvalue;
+
+}
+
+__global__ void MatrixSumKernel_2(int M, float* A_dev, float* SumPar_dev) {
+
+	float tmpValue = 0;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if (index < M){
+		int offset = index* M;
+		int k = 0;
+		for(; k< M;k++){
+			tmpValue = tmpValue + A_dev[offset + k];
+			printf("OFFSET: %d :::: K: %d :::: A_dev %f\n",offset,k,A_dev[offset + k]); 
+		}
+		SumPar_dev[index] = tmpValue;
+	}
+
+}
+
+// EJERCICIO 2
+__global__ void SumaColMatrizKernel2(int M, float* A_dev, float* SumPar_dev){
+
+	/*
+
+	se asume que
+	int chunk = 32;
+	dim3 tamGrid(1, N); //Grid dimensión, N bloques
+	dim3 tamBlock(chunk,1,1); //Block dimensión
+	for(int i =0; i<CANT_REPETICIONES ; i++)
+	SumaColMatrizKernel<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+
+	*/
+
+	float tmpValue = 0;
+	int size = M / blockDim.x;
+	int start = blockIdx.y * M + threadIdx.x * size;
+	int end = start + size;
+	int k = start;
+	for (; k < end; k++){
+		tmpValue = tmpValue + A_dev[k];
+	}
+
+	atomicAdd( & (SumPar_dev[ blockIdx.y ]) , tmpValue );
+
+}
+
+// EJERCICIO 3, Parte A
+__global__ void SumaColMatrizKernel3A(int M,float* A_dev, float* SumPar_dev){
+	// M = filas
+
+	/*
+
+	se asume que
+	int chunk = 32;
+	dim3 tamGrid(1, N); //Grid dimensión, N bloques
+	dim3 tamBlock(chunk,1,1); //Block dimensión
+	for(int i =0; i<CANT_REPETICIONES ; i++)
+	SumaColMatrizKernel<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+	x
+	
+	*/
+
+	float tmpValue = 0;
+	int columna = blockIdx.y * M;
+	int chunk2 = 1;//blockDim.x;
+	
+	
+	//printf(" threadIdx.x: %i , top: %i", threadIdx.x, top);
+
+	//  Coalesced 
+	for (int i = 0; i < chunk2; i++) {
+		tmpValue = tmpValue + A_dev[ columna + threadIdx.x + i*chunk2 ];
+		printf("Thread: %d :::: i: %d :::: Indice: %d :::: m: %d :::: BlockIdx.y: %d :::: A_dev: %f\n",threadIdx.x,i,columna + threadIdx.x + i*chunk2,M,blockIdx.y, A_dev[ columna + threadIdx.x + i*chunk2 ] );  
+	}
+
+	atomicAdd( &( SumPar_dev[ blockIdx.y ] ), tmpValue );
+	
+}
+// EJERCICIO 3, Parte b
+__global__ void SumaColMatrizKernel3(int M,float* A_dev, float* SumPar_dev){
+	// M = filas
+
+	/*
+
+	int chunk = 2;
+	dim3 tamGrid(M/chunk, N/chunk); //Grid dimensión
+	dim3 tamBlock(chunk,1,1); //Block dimensión
+	// lanzamiento del kernel
+	for(int i =0; i<CANT_REPETICIONES ; i++)
+	SumaColMatrizKernel<<<tamGrid, tamBlock>>>(M, Md, Nd);
+
+	4 * 4 , -> 2.x2 , 4 bloques
+
+	*/
+
+	float tmpValue = 0;
+
+	int start =  gridDim.x * blockIdx.x * M + threadIdx.x * M + blockIdx.y*2;
+	int end = start + 2;
+	
+	for(int k = start; k < end ; k++){
+		tmpValue = tmpValue + A_dev[k];
+	}
+
+	printf("value %f \n", tmpValue);
+
+	atomicAdd( &( SumPar_dev[ blockDim.x * blockIdx.x + threadIdx.x ] ), tmpValue );
+}
+
+
+
+
+//extern "C"
+float sumaColMatriz(int M, int N, float * A_hst, int algoritmo){
+
+
+	size_t size = M * N * sizeof(float);
+	size_t size2 = N*sizeof(float);
+
+	float* A_dev, *SumPar_dev;
+
+	float *SumPar_hst = (float *)malloc(N*sizeof(float));
+
+	// Allocate en device
+	cudaMalloc(&A_dev, size);
+	cudaMalloc(&SumPar_dev, size2);
+
+	// Inicializo matrices en el device
+	clockStart();
+	cudaMemcpy(A_dev, A_hst, size, cudaMemcpyHostToDevice);
+	cudaMemset(SumPar_dev,0, size2);
+	clockStop("transf CPU -> GPU");
+
+	clockStart();
+
+	switch(algoritmo) {
+	case 1: {
+
+		//Configurar la grilla
+		dim3 tamGrid (1, 1); //Grid dimensi? 1x1, Tiene un bloque de ejecucion
+		dim3 tamBlock(1, N); //Block dimensi? 1 x N, Hay N threads por bloque, threadId.x = 0, threadId.y esta en [0 .. N -1]
+
+		for(int i = 0; i < CANT_REPETICIONES; i++)
+			MatrixSumKernel_1<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+
+		break;
+
+	}
+	case 2: { // EJERCICIO 1
+		int threadsSize = 1024; // DESVENTAJA: Threads sin utilizar, hacer chequeos extras
+		dim3 tamGrid( (N / threadsSize ) + N % threadsSize == 0 ?  0 : 1, 1);
+
+		// dispongo bloques horizontalmente
+		dim3 tamBlock( threadsSize,  1);
+			   // dispongo threads horizontalmente
+
+			   for(int i = 0; i < CANT_REPETICIONES; i++)
+				   MatrixSumKernel_2<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+
+			   break;
+
+
+	}
+	case 3:{ // EJERCICIO 2
+
+		//printf("\n\nNo implementadoooooo!! :)\n\n\n");
+
+		// configuración de la ejecución
+		int newChunk = 32;
+		dim3 tamGrid(1, N); //Grid dimensión, N bloques
+		dim3 tamBlock(newChunk,1,1); //Block dimensión
+
+		// lanzamiento del kernel
+		for(int i = 0; i < CANT_REPETICIONES; i++) {
+			SumaColMatrizKernel2<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+		}
+		//...
+
+		break;
+
+	}
+	case 4: { // EJERCICIO 3.a
+
+		int newChunk = 4;
+		dim3 tamGrid(1, N); //Grid dimensión, N bloques
+		dim3 tamBlock(newChunk,1,1); //Block dimensión
+		
+		// lanzamiento del kernel
+		for (int i = 0; i < CANT_REPETICIONES; i++){
+			SumaColMatrizKernel3A<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+		}
+			   
+
+			   break;
+
+			}
+
+				   case 5: { // EJERCICIO 3.a
+
+			   int newChunk = 2;
+			   dim3 tamGrid(M / newChunk, N/ newChunk); //Grid dimensión, N bloques
+			   dim3 tamBlock(newChunk,1,1); //Block dimensión
+			   // lanzamiento del kernel
+
+
+			   for(int i =0; i<CANT_REPETICIONES ; i++){
+				   SumaColMatrizKernel3<<<tamGrid, tamBlock>>>(M, A_dev, SumPar_dev);
+			   }
+			   
+
+			   break;
+
+			}
+
+
+	}
+
+	
+	//print_matrix(A_hst,M);
+
+
+	cudaDeviceSynchronize();
+	clockStop("kernel");
+
+	// Traer resultado;
+	clockStart();
+	cudaMemcpy(SumPar_hst, SumPar_dev, size2, cudaMemcpyDeviceToHost);
+	clockStop("transf CPU <- GPU");
+
+	// Sumar el vector de resultados parciales;
+	float total = 0.0;
+	total = sum_matrix(SumPar_hst,N);
+
+	free(SumPar_hst);
+	// Free matrices en device
+	cudaFree(A_dev); cudaFree(SumPar_dev);
+
+	return total;
+
+
+}
+
+
+int main(int argc, char** argv){
+
+	if (argc < 3){
+		printf("Uso:\nMatSum n algo(1:3)");
+		exit(0);
+	}
+
+	int n= atoi(argv[1]);
+	int algo = atoi(argv[2]);
+
+	float *A = (float *)malloc(n*n*sizeof(float));
+
+	init_matrix(A,n);
+
+	clockStart();
+	float result_ref = sum_matrix(A,n,n);
+	clockStop("CPU");
+
+	printf("algo - %i \n",algo);
+
+	float result_gpu = sumaColMatriz(n,n,A,algo);
+
+	printf("res_cpu - %f / res_gpu - %f \n",result_ref, result_gpu);
+
+	if (result_gpu == result_ref)
+		printf("\n\nResultado OK!! :)\n\n\n");
+	else
+		printf("\n\Segui participando\n\n\n");
+
+	free(A);
+
+	char c = getchar();
+
+	printf("%i",c);
+
+	return 0;
+}
+
+
