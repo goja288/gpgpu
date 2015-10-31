b57777345afc68444515c170975c69e4b73e6cc7
goja288 <goja288@gmail.com>
2015-10-31 16:40:30 -0300

    Unificando y haciendo la parte3

--- laboratorio/Ejercicio 2/Voronoi/kernel.cu
+++ laboratorio/Ejercicio 2/Voronoi/kernel.cu
@@ -3,12 +3,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "CImg.h"
+#include "mock.h"
 
 using namespace cimg_library;
 using namespace std;
 
 #define CHUNK 32
 #define MASKSIZE 5
+
+__global__ void kernelParte2(float* input, float* ouput, int imgWidth, int imgHeight) {
+
+	__shared__ float maskMemShared[ ((int)(MASKSIZE / 2) * 2 + CHUNK) * ((int)(MASKSIZE / 2) * 2 + CHUNK)];
+
+	/******************* CARGA DE MEMORIA COMPARTIDA *************/
+
+	int maskPadding = (int)(MASKSIZE / 2),
+	    maskDim  = maskPadding * 2 + CHUNK, // maskDim x maskDim
+
+	    column =  threadIdx.x,
+	    row = threadIdx.y,
+
+	    globalColumn = blockIdx.x * blockDim.x  + column,
+	    globalRow = blockIdx.y * blockDim.y  + row,
+
+	    startColumn = globalColumn - maskPadding,
+	    startRow    = globalRow - maskPadding,
+
+	    iColumn = startColumn,
+	    iRow = 0,
+
+	    iMask = startRow,
+	    jMask = startColumn,
+
+	    i, iTope,
+	    j, jTope;
+
+	float avg = 0;
+
+	for ( iRow  = row; iRow < maskDim; iRow += CHUNK ) {
+		jMask = startColumn;
+		for ( iColumn = column ; iColumn < maskDim; iColumn += CHUNK) {
+
+			if (jMask < 0 || iMask < 0 || jMask >= imgWidth || iMask >= imgHeight) {
+				maskMemShared[ iRow * maskDim + iColumn ] = 0;
+			} else {
+				maskMemShared[ iRow * maskDim + iColumn] =  input[imgWidth * iMask + jMask];
+			}
+			jMask += CHUNK;
+		}
+		iMask += CHUNK;
+	}
+
+	__syncthreads();
+	/*
+	if (column == 0 && row == 0 && blockIdx.x == 2 && blockIdx.y == 0) {
+
+		printf("%d  %d  \n ", globalColumn, globalRow);
+
+		for (int i = 0; i < maskDim ; i++) {
+			for (int j = 0; j < maskDim ; j++) {
+				printf("%f ", maskMemShared[i * maskDim + j]);
+			}
+			printf("\n");
+		}
+
+	}
+	*/
+
+
+	/************** CUENTAS ***************/
+
+	if (globalColumn < imgWidth  && globalRow < imgHeight) {
+
+		i = row;
+		j = column;
+
+		iTope = i + MASKSIZE;
+		jTope = j + MASKSIZE;
+
+		for ( ; i < iTope; i++) {
+			for ( j = column; j < jTope; j++) {
+				avg += maskMemShared[ i * maskDim + j];
+			}
+		}
+		startRow += maskPadding;
+		startColumn += maskPadding;
+
+		ouput[imgWidth * startRow + startColumn] = avg / (MASKSIZE * MASKSIZE);
+
+	}
+}
+
+__global__ void kernelParte3(float* imagenPromedio, int* a_centros, float voronoiOutput, int width, int height) {
+
+}
 
 // Retorna un array con la posicion de cada centro
 int* sorteoCentros(int cantCentros, int width, int height) {
@@ -59,14 +147,14 @@ int* sorteoCentros(int cantCentros, int width, int height) {
 /**
  * Funciones auxiliares
  */
+void cudaCheck()
+{
+	cudaError_t cudaError;
 
-void imprimirMatriz(float* m, int width, int height){
-	int i, j;
-	for(j =0; j < height; j++){
-		for(i=0;i < width; i++){
-			fprintf(stderr, "%.2f  ", m[ j*width + i] );
-		}
-		fprintf(stderr, "\n" );
+	cudaError = cudaGetLastError();
+	if (cudaError != cudaSuccess)
+	{
+		printf("  cudaGetLastError() returned %d: %s\n", cudaError, cudaGetErrorString(cudaError));
 	}
 }
 
@@ -87,8 +175,6 @@ int main()
 	float* img_matrix = image.data();
 
 	sorteoCentros(42,width,height);
-
-	//imprimirMatriz(img_matrix,width, height);
 
 	/**
 	 * Parte 1 - Secuencial
@@ -155,11 +241,63 @@ int main()
 
 
 	/**
-	 * Parte 3 - Secuencial
+	 * Parte 2 
+	 */
+	
+	/*************************** PREPARO TEST ***********************************/
+
+	float* testAVG = testear(img_matrix, width, height, MASKSIZE);
+
+	/*************************** RESERVA DE MEMORIA *****************************/
+
+	input_img_dev;
+	output_img_dev;
+	img_matrix_output = (float*)malloc( img_matrix_size );
+
+	cudaMalloc(&input_img_dev, img_matrix_size);
+	cudaMalloc(&output_img_dev, img_matrix_size);
+
+
+	cudaMemset(output_img_dev, 0, img_matrix_size);
+	cudaMemcpy(input_img_dev, img_matrix, img_matrix_size, cudaMemcpyHostToDevice);
+
+	/************************* AJUSTE DE INVOCACION *********************/
+
+	dim3 gridDimension( (int)( width / CHUNK) + (width % CHUNK == 0 ? 0 : 1), (int)(height / CHUNK ) + (height % CHUNK == 0 ? 0 : 1) );
+	dim3 blockDimension(CHUNK, CHUNK);
+
+	kernelParte2 <<< gridDimension, blockDimension>>>(input_img_dev, output_img_dev, width, height);
+	cudaCheck();
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(img_matrix_output, output_img_dev, img_matrix_size, cudaMemcpyDeviceToHost);
+	compareArray(testAVG, img_matrix_output, width, height);
+
+	//result.save_png("resultado.png");
+	
+	CImgDisplay parte2(result,"Parte2");
+	
+	while (!parte2.is_closed()) {
+		parte2.wait();
+	}
+
+	cudaFree(input_img_dev);
+	cudaFree(output_img_dev);
+
+	//free(img_matrix);
+	free(testAVG);
+	free(img_matrix_output);
+	
+	/**
+	 * Fin Parte 2 
 	 */
 
 	/**
-	 * FIN Parte 3 - Secuencial
+	 * Parte 3
+	 */
+
+	/**
+	 * FIN Parte 3
 	 */
 
 
